063c07b0a39e979d6e9e734bd57bef3dcbc8b721
Lucas Bohorquez <lucas.1701716343@gmail.com>
2022-05-20 11:19:24 -0500

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Lucas Bohorquez on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
